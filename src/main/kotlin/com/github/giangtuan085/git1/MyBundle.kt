6e0f436546822e4a1ebe9586b0c010678ae498fa
GitHub Action <action@github.com>
2024-12-04 12:08:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/giangtuan085/git1/MyBundle.kt
+++ src/main/kotlin/com/github/giangtuan085/git1/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.giangtuan085.git1
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/giangtuan085/git1/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/giangtuan085/git1/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.giangtuan085.git1.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
